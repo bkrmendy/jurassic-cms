b394409adab7dbe7aeac6b0ead3e96dc6d77d4b7
bkrmendy <korber314@gmail.com>
2023-11-24 12:52:13 +0100

    tweaks

--- deps.ts
+++
@@ -1,2 +0,0 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
-export { Router, Application };
--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 import { vercelStegaCombine, vercelStegaSplit } from "npm:@vercel/stega";
-import { oakCors } from "https://deno.land/x/cors/mod.ts";
-import type { Response } from "https://deno.land/x/oak/mod.ts";
+import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
+import type { Response } from "https://deno.land/x/oak@v12.6.1/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 import { Client } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
-import { Router, Application } from "./deps.ts";
 
 const status200 = <T extends Response["body"]>(response: Response, body: T) => {
   response.status = 200;
@@ -10,7 +10,7 @@ const status200 = <T extends Response["body"]>(response: Response, body: T) => {
 };
 
 const status400 = (response: Response, message: string) => {
-  response.status = 200;
+  response.status = 400;
   response.body = { success: false, message: message };
 };
 
@@ -82,7 +82,7 @@ router.get("/api/:project_id/:key", async ({ response, params }) => {
     status400(response, "Key not found");
     return;
   }
-  status200(response, vercelStegaCombine(result, { key }));
+  status200(response, vercelStegaCombine(result, { project_id, key }));
 });
 
 router.post("/api/:project_id/:key", async ({ request, response, params }) => {
@@ -91,7 +91,7 @@ router.post("/api/:project_id/:key", async ({ request, response, params }) => {
     return;
   }
 
-  const payload = await request.body().value;
+  const payload = await request.body({ type: "json" }).value;
   if (!("value" in payload)) {
     status400(response, "The request body must have a data prop");
     return;
